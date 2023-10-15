5da6468224908f3e449c962f15290ee18ec40883
Websk53 <manish.singarapu@gmail.com>
2023-10-15 18:47:27 -0400

    Angular - Http Clientand get request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './Models/Product';
+import { Pagination } from './Models/Pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-Commerce';
+  products: Product[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pagesize=50').subscribe({
+      next: response => this.products = response.data, //next operation
+      error: error => console.log(error), //error
+      complete: () => {
+        console.log("request completed");
+      }
+    })
+  }
 }
